c0a0cdeb351c3809648d4e75f534d1ae3019a705
chenlighten <chenlighten@gmail.com>
2022-10-11 15:58:40 -0400

    feat: more implementation for api

--- src/api.cpp
+++ src/api.cpp
@@ -32,6 +32,7 @@ DefineHttpHandler(UsersRegister) {
 
     try {
         const auto json_body = nlohmann::json::parse(req.body);
+        // to be modified: get from basic auth or some auth
         user_name = json_body.at("name");
         user_passwd = json_body.at("passwd");
         user_email = json_body.at("email");
@@ -67,6 +68,7 @@ DefineHttpHandler(UsersLogin) {
 
     try {
         const auto json_body = nlohmann::json::parse(req.body);
+        // to be modified: get from basic auth or some auth
         user_passwd = json_body.at("passwd");
         user_email = json_body.at("email");
     } catch (std::exception& e) {
@@ -90,6 +92,10 @@ DefineHttpHandler(UsersLogout) {
 
     // do something with token
     const std::string decoded_token = base64_decode(token);
+    // get user_id from decoded_token, format to be discussed
+    // const std::string user_id = some_function(decoded_token);
+
+    // do something with user_id
     
     result["msg"] = "success";
     res.set_content(result.dump(), "text/plain");
@@ -109,13 +115,48 @@ DefineHttpHandler(TaskLists) {
 
     // do something with token
     const std::string decoded_token = base64_decode(token);
+    // get user_id from decoded_token, format to be discussed
+    // const std::string user_id = some_function(decoded_token);
+
+    // do something with user_id
     
     result["msg"] = "success";
     res.set_content(result.dump(), "text/plain");
 }
 
 DefineHttpHandler(TaskListsCreate) {
-    std::cout << "Request body: " << req.body;
+    // do it later
+}
+
+DefineHttpHandler(Tasks) {
+    const std::vector<std::string> splited_path = Utils::Split(req.path, "/");
+
+    // some ugly handling for unresolved path
+
+    if (splited_path.size() > 0 && splited_path.back() == "create") {
+        TasksCreate(req, res);
+        return;
+    }
+
+    if (splited_path.size() < 3) {
+        // bad path
+    }
+
+    std::string user_id;
+    std::string task_list_name = *(splited_path.rbegin() + 2);
+    std::string task_name = splited_path.back();
+
+    // do some logic
+}
+
+DefineHttpHandler(TasksCreate) {
+    const std::vector<std::string> splited_path = Utils::Split(req.path, "/");
+
+    std::string user_id;
+    std::string task_list_name = *(splited_path.rbegin() + 2);
+    std::string task_name;
+
+    // unfinised, do it later
 }
 
 void API::Run(const std::string& host, uint32_t port) {
@@ -125,7 +166,6 @@ void API::Run(const std::string& host, uint32_t port) {
     AddHttpHandler(svr, "/v1/task_lists/", Get, TaskLists);
     AddHttpHandler(svr, "/v1/task_lists/create", Post, TaskListsCreate);
 
-    svr->Get("/stop", [this](const httplib::Request& req, httplib::Response& res){this->Stop();});
     svr->listen(host, port);
 }
 
