7cf274e3669806e7212be78e5588c3d9f2885410
xxcisxxc <w5966466@163.com>
2022-10-25 03:36:53 +0000

    Format main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,24 @@
-#include <common/utils.h>
-#include <api/api.h>
 #include "db/DB.h"
+#include <api/api.h>
+#include <common/utils.h>
 #include <memory>
 #include <string>
 
 int main(void) {
-    std::string api_host = Common::GetEnv<std::string>("api_host");
-    uint32_t api_port = Common::GetEnv<uint32_t>("api_port");
+  std::string api_host = Common::GetEnv<std::string>("api_host");
+  uint32_t api_port = Common::GetEnv<uint32_t>("api_port");
 
-    if (api_host.empty()) {
-        api_host = "0.0.0.0";
-    }
-    if (!api_port) {
-        api_port = 3001;
-    }
+  if (api_host.empty()) {
+    api_host = "0.0.0.0";
+  }
+  if (!api_port) {
+    api_port = 3001;
+  }
 
-    const std::string db_host = "neo4j://neo4j:hello4156@localhost:7687";
-    auto db_instance = std::make_shared<DB>(db_host);
+  const std::string db_host = "neo4j://neo4j:hello4156@localhost:7687";
+  auto db_instance = std::make_shared<DB>(db_host);
 
-    API api(nullptr, nullptr, nullptr, db_instance, nullptr);
-    api.Run(api_host, api_port);
-    return 0;
+  API api(nullptr, nullptr, nullptr, db_instance, nullptr);
+  api.Run(api_host, api_port);
+  return 0;
 }
