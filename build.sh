1cd44f4460a1cb5c26e740ef26a703255a5e8405
xxcisxxc <32963789+xxcisxxc@users.noreply.github.com>
2022-11-24 12:56:43 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 sudo apt update
-sudo apt install build-essential git cmake libssl-dev autoconf libtool clang-format libcypher-parser-dev libedit-dev libssl-dev -y
+sudo apt install build-essential git cmake libssl-dev autoconf libtool clang-format libcypher-parser-dev libedit-dev -y
 git submodule update --init
 cd external/googletest && mkdir build && cd build && cmake .. && make && sudo make install && cd ../../..
 cd external/libneo4j-client && ./autogen.sh && ./configure --disable-werror && make clean check && sudo make install && cd ../../..
@@ -16,7 +16,7 @@ else
     curl -fsSL https://get.docker.com | bash -s docker
 fi
 
-if [ $1 == "build" ]; then
+if [ "$1" == "build" ]; then
     docker run -d -p7474:7474 -p7687:7687 -e NEO4J_AUTH=neo4j/hello4156 neo4j:4.4.9
     sleep 10
     mkdir build && cd build && cmake .. && make
