617148b21adcfa1c83be009e1849688e10c323d5
xxcisxxc <32963789+xxcisxxc@users.noreply.github.com>
2022-11-24 11:54:05 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,21 @@ sudo apt install build-essential git cmake libssl-dev autoconf libtool clang-for
 git submodule update --init
 cd external/googletest && mkdir build && cd build && cmake .. && make && sudo make install && cd ../../..
 cd external/libneo4j-client && ./autogen.sh && ./configure --disable-werror && make clean check && sudo make install && cd ../../..
-curl -fsSL https://get.docker.com | bash -s docker
-docker run -d -p7474:7474 -p7687:7687 -e NEO4J_AUTH=neo4j/hello4156 neo4j:4.4.9
-sleep 10
-echo "/usr/local/lib" | sudo tee -a /etc/ld.so.conf && sudo ldconfig
-mkdir build && cd build && cmake .. && make
+if [ -x "$(command -v neo4j-client)" ]; then
+    echo "Env already set"
+else
+    echo "Set Env"
+    echo "/usr/local/lib" | sudo tee -a /etc/ld.so.conf && sudo ldconfig
+fi
+if [ -x "$(command -v docker)" ]; then
+    echo "Skip install docker"
+else
+    echo "Install docker"
+    curl -fsSL https://get.docker.com | bash -s docker
+fi
+
+if [ $1 == "build" ]; then
+    docker run -d -p7474:7474 -p7687:7687 -e NEO4J_AUTH=neo4j/hello4156 neo4j:4.4.9
+    sleep 10
+    mkdir build && cd build && cmake .. && make
+fi
