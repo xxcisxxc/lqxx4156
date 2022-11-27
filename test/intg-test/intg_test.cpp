ce33afcf20e1b584144d690898c8a4772c44f8ac
Kenny-ting <liujiting0501@icloud.com>
2022-11-27 01:26:45 -0500

    add Users Delete function

--- test/intg-test/intg_test.cpp
+++ test/intg-test/intg_test.cpp
@@ -30,88 +30,85 @@
 //   std::shared_ptr<Users> users;
 // };
 
-
 // /*
 //  * @brief Integration test for users
 //  */
 
 // TEST_F(IntgTest, Users_Create) {
 //   // should be able to create a user
-//   EXPECT_TRUE(users->Create(UserInfo("alice", "alice@columbia.edu", "123456")));
-//   EXPECT_TRUE(users->Create(UserInfo("bob", "bob@columbia.edu", "123456")));
-//   EXPECT_TRUE(
+//   EXPECT_TRUE(users->Create(UserInfo("alice", "alice@columbia.edu",
+//   "123456"))); EXPECT_TRUE(users->Create(UserInfo("bob", "bob@columbia.edu",
+//   "123456"))); EXPECT_TRUE(
 //       users->Create(UserInfo("alice2", "alice2@columbia.edu", "123456")));
-  
+
 //   // duplicate email failed
 //   EXPECT_FALSE(
 //       users->Create(UserInfo("alice3", "alice@columbia.edu", "123456")));
-  
+
 //   // email format failed
 //   EXPECT_FALSE(
 //       users->Create(UserInfo("bob", "bob@", "123456")));
-  
-//   // delete user
 
+//   // delete user
 
 // };
 
 // TEST_F(IntgTest, Users_Delete) {
 //   // should be able to create a user
-//   EXPECT_TRUE(users->Delete(UserInfo("alice", "alice@columbia.edu", "123456")));
-  
+//   EXPECT_TRUE(users->Delete(UserInfo("alice", "alice@columbia.edu",
+//   "123456")));
+
 //   // duplicate email failed
 //   EXPECT_FALSE(
 //       users->Delete(UserInfo("alice3", "alice@columbia.edu", "123456")));
-  
+
 //   // no such user
 //   EXPECT_FALSE(
 //       users->Delete(UserInfo("bob", "bob@", "123456")));
-  
-//   // delete user
 
+//   // delete user
 
 // };
 
 // TEST_F(IntgTest, Users_Validate) {
 //   // no such user
-//   EXPECT_FALSE(users->Validate(UserInfo("", "alice@columbia.edu", "123456")));
+//   EXPECT_FALSE(users->Validate(UserInfo("", "alice@columbia.edu",
+//   "123456")));
 
 //   // should be able to validate a user
-//   EXPECT_TRUE(users->Create(UserInfo("alice", "alice@columbia.edu", "123456")));
-//   EXPECT_TRUE(users->Validate(UserInfo("", "alice@columbia.edu", "123456")));
-//   EXPECT_TRUE(
+//   EXPECT_TRUE(users->Create(UserInfo("alice", "alice@columbia.edu",
+//   "123456"))); EXPECT_TRUE(users->Validate(UserInfo("", "alice@columbia.edu",
+//   "123456"))); EXPECT_TRUE(
 //       users->Validate(UserInfo("alice", "alice@columbia.edu", "123456")));
-  
+
 //   // wrong name
 //   EXPECT_FALSE(
-//       users->Validate(UserInfo("not alice", "alice@columbia.edu", "123456")));
+//       users->Validate(UserInfo("not alice", "alice@columbia.edu",
+//       "123456")));
 //   // wrong password
 //   EXPECT_FALSE(users->Validate(
 //       UserInfo("alice", "alice@columbia.edu", "wrong password")));
-  
+
 //   // missing password
 //   EXPECT_FALSE(users->Validate(UserInfo("", "alice@columbia.edu", "")));
-//   EXPECT_FALSE(users->Validate(UserInfo("alice", "alice@columbia.edu", "")));  
+//   EXPECT_FALSE(users->Validate(UserInfo("alice", "alice@columbia.edu", "")));
 
 //   // delete user
-
 
 // };
 
 // TEST_F(IntgTest, Users_DuplicatedEmail) {
 //   // should be successful
-//   EXPECT_TRUE(users->Create(UserInfo("alice", "alice@columbia.edu", "123456")));
+//   EXPECT_TRUE(users->Create(UserInfo("alice", "alice@columbia.edu",
+//   "123456")));
 //   EXPECT_TRUE(users->DuplicatedEmail(UserInfo("alice@columbia.edu")));
-  
+
 //   // no such user
 //   EXPECT_FALSE(users->DuplicatedEmail(UserInfo("bob@columbia.edu")));
 
 //   // delete user
 
-
-
 // };
-
 
 // /*
 //  * @brief Integration test for TaskLists
@@ -121,8 +118,6 @@
 //   // setup environment
 //   RequestData data;
 //   TasklistContent out;
-
-
 
 // };
 
@@ -135,39 +130,39 @@
 // };
 
 // TEST_F(IntgTest, TaskLists_Delete) {
-  
+
 // };
 
 // TEST_F(IntgTest, TaskLists_Revise) {
-  
+
 // };
 
 // TEST_F(IntgTest, TaskLists_GetAllTasklist) {
-  
+
 // };
 
 // TEST_F(IntgTest, TaskLists_GetAllAccessTaskList) {
-  
+
 // };
 
 // TEST_F(IntgTest, TaskLists_GetAllGrantTaskList) {
-  
+
 // };
 
 // TEST_F(IntgTest, TaskLists_ReviseGrantTaskList) {
-  
+
 // };
 
 // TEST_F(IntgTest, TaskLists_RemoveGrantTaskList) {
-  
+
 // };
 
 // TEST_F(IntgTest, TaskLists_GetAllPublicTaskList) {
-  
+
 // };
 
 // TEST_F(IntgTest, TaskLists_Exists) {
-  
+
 // };
 
 // /*
